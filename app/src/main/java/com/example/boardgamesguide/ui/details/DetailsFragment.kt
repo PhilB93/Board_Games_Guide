40169cc24cd89594182cbe984626c29ade7189f9
PhilB93 <F19931993b>
2021-11-25 16:50:01 +0300

    Use WebView In DetailsFragment.kt

--- app/src/main/java/com/example/boardgamesguide/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/boardgamesguide/ui/details/DetailsFragment.kt
@@ -3,6 +3,7 @@ package com.example.boardgamesguide.ui.details
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -19,7 +20,7 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
-    // private val detailsViewModel by viewModels<DetailsViewModel>()
+
     private val binding: FragmentDetailsBinding by viewBinding()
     private val args: DetailsFragmentArgs by navArgs()
 
@@ -28,32 +29,36 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setGameView()
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun setGameView() {
-        with(binding)
-        {
-
-            val game = args.game
-            Glide.with(ivGamePic).load(game.image_url).apply(
-                RequestOptions()
-                    .placeholder(R.drawable.ic_search)
-                    .error(R.drawable.ic_search)
-                    .transform(RoundedCorners(ROUNDED_CORNERS))
-            ).into(ivGamePic)
-            tvPlayersInfo.text = "${game.min_players} - ${game.max_players}"
-            tvAgesInfo.text = "${game.min_age}+"
-            tvPlaytimeInfo.text = "${game.min_playtime} - ${game.max_playtime} mins"
-            tvPriceInfo.text = game.price_text
-            tvRatingInfo.text = String.format("%.3f", game.average_user_rating)
-            tvNameInfo.text = game.name
-            (if (game.description_preview.isNullOrEmpty())
-                game.description else game.description_preview)
-                .also { tvDescription.text =
-                it}
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(args.game.url)
         }
-
+       // setGameView()
     }
+
+//    @SuppressLint("SetTextI18n")
+//    private fun setGameView() {
+//        with(binding)
+//        {
+//
+//            val game = args.game
+//            Glide.with(ivGamePic).load(game.image_url).apply(
+//                RequestOptions()
+//                    .placeholder(R.drawable.ic_search)
+//                    .error(R.drawable.ic_search)
+//                    .transform(RoundedCorners(ROUNDED_CORNERS))
+//            ).into(ivGamePic)
+//            tvPlayersInfo.text = "${game.min_players} - ${game.max_players}"
+//            tvAgesInfo.text = "${game.min_age}+"
+//            tvPlaytimeInfo.text = "${game.min_playtime} - ${game.max_playtime} mins"
+//            tvPriceInfo.text = game.price_text
+//            tvRatingInfo.text = String.format("%.3f", game.average_user_rating)
+//            tvNameInfo.text = game.name
+//            (if (game.description_preview.isNullOrEmpty())
+//                game.description else game.description_preview)
+//                .also { tvDescription.text =
+//                it}
+//        }
+//
+//    }
 }
